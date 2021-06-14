47bda180cb0bb95830568c4e548e309b735ab553
tcak76 <tcak76@users.noreply.github.com>
2021-06-14 15:23:31 +0300

    first commit

--- Example/Pods/Target Support Files/Pods-j2objc_Tests/Pods-j2objc_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_j2objc_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_j2objc_Tests
-@end
--- Example/Pods/Target Support Files/j2objc/j2objc-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_j2objc : NSObject
-@end
-@implementation PodsDummy_j2objc
-@end
